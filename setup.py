0097dfbdaf491e6ebc792cc7f2cabc5a6c94b1d5
Harshan <harshanrk321@gmail.com>
2026-05-03 22:51:21 +0530

    New Commit

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str) -> List[str]:
+    with open(file_path) as f:
+        requirements = f.readlines()
+    requirements = [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+    return requirements
 
 setup(
     name='mlProject',
-    version='0.0.1'
+    version='0.0.1',
+    author="Harshan R K ",
+    author_email="harshanrk3210@gmail.com",
+    packages=find_packages(),
+    install_requires= get_requirements('requirements.txt'),
 )
